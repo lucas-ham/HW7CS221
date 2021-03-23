945d493bf1a1463ee0a9c205da9ac9a3f6b40f2e
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-03-22 17:41:57 -0700

    Fixed some syntax issues

--- htree.cc
+++ htree.cc
@@ -11,19 +11,19 @@ HTree::possible_path_t
 HTree::path_to(key_t key) const
 {
   if (!this){
-    return nullptr
+    return nullptr;
   }
   if (this->get_key() == key){
     return {};
   }
   Direction dirLeft = LEFT;
-  const auto left_res = path_to(this.get_child(dirLeft), key);
+  const auto left_res = path_to(this->get_child(dirLeft), key);
   if (left_res) {
     std::list return_list = {dirLeft};
     return return_list.splice(return_list.end(), left_res);
   }
   Direction dirRight = RIGHT;
-  const auto right_res = path_to(this.get_child(dirRight), key);
+  const auto right_res = path_to(this->get_child(dirRight), key);
   if (right_res) {
     std::list return_list = {dirRight};
     return return_list.splice(return_list.end(), right_res);
