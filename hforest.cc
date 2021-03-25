f1fcee39053264333eb982870eaf204561ae23d7
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-03-24 17:53:25 -0700

    a

---
+++ hforest.cc
@@ -0,0 +1,8 @@
+#include "hforest.hh"
+
+
+Hforest::Hforest(size_t size)
+  :size(size)
+  {
+  heaparray = tree_ptr_t array [size];
+}
--- hforest.hh
+++ hforest.hh
@@ -8,15 +8,10 @@ class Hforest{
   public:
     using size_t = uint64_t;
     using tree_ptr_t = HTree::tree_ptr_t;
-    Hforest(size_t size){};
-    int size(){
-
-    }
-    void add_tree(tree_ptr_t tree){
-    }
-    tree_ptr_t pop_tree(){
-
-    }
+    Hforest(size_t size);
+    int size();
+    void add_tree(tree_ptr_t tree);
+    tree_ptr_t pop_tree();
   private:
     size_t size;
     tree_ptr_t heaparray [size];
