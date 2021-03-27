03fa241af8a05f8de8383306d6bdb99c123f36f6
Lucas Hamilton <lucas@macobserver.com>
2021-03-27 14:03:36 -0700

    Tried to make some more tests, also added identifiers to the tree. Line 39 is causing a segfault, not sure how else to test path_to on a non-trivial path? Does this mean our path_to function is bad?

--- test_htree.cc
+++ test_htree.cc
@@ -14,18 +14,18 @@ HTree::tree_ptr_t create_test_tree()
 {
   return
     make_shared<HTree>(126, 1,
-		make_shared<HTree>(50, 1,
-			make_shared<HTree>(30, 1, nullptr, 
-				make_shared<HTree>(50, 1)
-				),
-			make_shared<HTree>(20, 1,
-				make_shared<HTree>(40, 1,
-					make_shared<HTree>(10, 1),
-					make_shared<HTree>(5, 1)
+		make_shared<HTree>(50, 1,				//left child of root
+			make_shared<HTree>(30, 1, nullptr, 		//left child of left child of root
+				make_shared<HTree>(50, 1)		//left of left of left of root	
+				),	
+			make_shared<HTree>(20, 1,			//right of left of root
+				make_shared<HTree>(40, 1,		//left of right of left of root
+					make_shared<HTree>(10, 1),	//left of left of right of left of root
+					make_shared<HTree>(5, 1)	//right of left of right of left of root	
 					),
-				make_shared<HTree>(80, 1,
-					make_shared<HTree>(126, 1),
-					make_shared<HTree>(40, 1)
+				make_shared<HTree>(80, 1,		//right of right of left of root
+					make_shared<HTree>(126, 1),	//left of right of right of left of root
+					make_shared<HTree>(40, 1)	//right of right of right of left of root
 					)
 				)
 		    )
@@ -35,7 +35,15 @@ HTree::tree_ptr_t create_test_tree()
 void test_htree(const HTree::tree_ptr_t root)
 {
   assert(*(root->path_to(126)) == HTree::path_t({ }));
+
+  assert(*(root->path_to(50)) == HTree::path_t{ HTree::Direction::LEFT } ); 
+
+//  assert(*(root->path_to(40)) == HTree::path_t({HTree::Direction::RIGHT, HTree::Direction::LEFT}));
+
   assert((root->get_child(HTree::Direction::LEFT))->get_key()  == 50);
+
+  assert(((root->get_child(HTree::Direction::LEFT))->get_child(HTree::Direction::RIGHT))->get_key() == 20); 
+
 }
 
 int main()
