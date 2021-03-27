f78426bf0e015b56b7c8c0a379943393e3d01871
Lucas Hamilton <lucas@macobserver.com>
2021-03-27 13:40:10 -0700

    Created a sample tree with many children and some repeated keys for testing. Wrote one test

--- test_htree.cc
+++ test_htree.cc
@@ -5,18 +5,37 @@
 #include "htree.hh"
 #include <cassert>
 
+#include <iostream>
+
 using namespace std;
 
 // Create an elaborate example test tree:
 HTree::tree_ptr_t create_test_tree()
 {
   return
-    make_shared<HTree>(126, 1);
+    make_shared<HTree>(126, 1,
+		make_shared<HTree>(50, 1,
+			make_shared<HTree>(30, 1, nullptr, 
+				make_shared<HTree>(50, 1)
+				),
+			make_shared<HTree>(20, 1,
+				make_shared<HTree>(40, 1,
+					make_shared<HTree>(10, 1),
+					make_shared<HTree>(5, 1)
+					),
+				make_shared<HTree>(80, 1,
+					make_shared<HTree>(126, 1),
+					make_shared<HTree>(40, 1)
+					)
+				)
+		    )
+		);
 }
 
 void test_htree(const HTree::tree_ptr_t root)
 {
   assert(*(root->path_to(126)) == HTree::path_t({ }));
+  assert((root->get_child(HTree::Direction::LEFT))->get_key()  == 50);
 }
 
 int main()
